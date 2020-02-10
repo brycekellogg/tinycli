47ca0cfd8b5d0113c23d4717b22440db00ba683b
Bryce Kellogg <bryce@kellogg.org>
2020-02-10 11:13:35 -0800

    Re-organized test framework to be more flexible

--- test/main.c
+++ test/main.c
@@ -1,177 +1,64 @@
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "tinycli.h"
 
-int    check_i0;
-int    check_i1;
-double check_d0;
-double check_d1;
-int    check_s0;
-int    check_s1;
 
-int f_v(void) {
-    check_i0 = 0;
-    return 1;
+typedef struct {
+    char*  testname;
+    char*  inputStr;
+    char*  expectedEchoStr;
+    int    expectedArgsInt[TINYCLI_MAXARGS];
+    char   expectedArgsStr[TINYCLI_MAXARGS][TINYCLI_MAXBUFFER];
+    double expectedArgsDouble[TINYCLI_MAXARGS];
+    int    expectedResult;
+} testcase;
+
+int    argsInt[TINYCLI_MAXARGS];
+char   argsStr[TINYCLI_MAXARGS][TINYCLI_MAXBUFFER];
+double argsDouble[TINYCLI_MAXARGS];
+
+#include "testcases.h"
+#include "testfunctions.h"
+
+void runTest(testcase t) {
+    for (int i = 0; i < TINYCLI_MAXARGS; i++) {
+        argsInt[i] = 0;
+    }
+
+    for (int i = 0; i < TINYCLI_MAXARGS; i++) {
+        argsDouble[i] = 0;
+    }
+
+    for (int i = 0; i < TINYCLI_MAXARGS; i++) {
+        argsStr[i][0] = '\0';
+    }
+
+    tinycli_putsn(t.inputStr, strlen(t.inputStr));   // write command string
+   
+    assert(tinycli_result == t.expectedResult);
+
+    for (int i = 0; i < TINYCLI_MAXARGS; i++) {
+        assert(argsInt[i] == t.expectedArgsInt[i]);
+    }
+
+    for (int i = 0; i < TINYCLI_MAXARGS; i++) {
+        assert(argsDouble[i] == t.expectedArgsDouble[i]);
+    }
+
+    for (int i = 0; i < TINYCLI_MAXARGS; i++) {
+        assert(0 == strcmp(argsStr[i], t.expectedArgsStr[i]));
+    }
 }
 
-int f_i(int arg0) {
-    check_i0 = arg0;
-    return 2;
-}
-
-int f_d(double arg0) {
-    check_d0 = arg0;
-    return 3;
-}
-
-int f_ii(int arg0, int arg1) {
-    check_i0 = arg0;
-    check_i1 = arg1;
-    return 4;
-}
-
-int f_id(int arg0, double arg1) {
-    check_i0 = arg0;
-    check_d1 = arg1;
-    return 5;
-}
-
-int f_di(double arg0, int arg1) {
-    check_d0 = arg0;
-    check_i1 = arg1;
-    return 6;
-}
-
-int f_dd(double arg0, double arg1) {
-    check_d0 = arg0;
-    check_d1 = arg1;
-    return 7;
-}
-
-int f_s(char* arg0) {
-    check_s0 = strcmp(arg0, "helloWorld");
-    check_s1 = strcmp(arg0, "abc");
-    return 1;
-}
 
 int main() {
-
-    /* Test void function */
-    check_i0 = -1;            /* init check variables */
-    check_i1 = -1;
-    check_d0 = -1;
-    check_d1 = -1;
-    tinycli_putsn("v\r", 2);        /* write command string */
-    assert(tinycli_result == 1);
-    assert(check_i0 == 0);    /* test check variables */
-    assert(check_i1 == -1);
-    assert(check_d0 == -1);
-    assert(check_d1 == -1);
-
-
-    /* Test int function */
-    check_i0 = -1;            /* init check variables */
-    check_i1 = -1;
-    check_d0 = -1;
-    check_d1 = -1;
-    tinycli_putsn("i 13\r", 5);     /* write command string */
-    assert(check_i0 == 13);   /* test check variables */
-    assert(check_i1 == -1);
-    assert(check_d0 == -1);
-    assert(check_d1 == -1);
-   
-
-    /* Test int function (hex) */
-    check_i0 = -1;            /* init check variables */
-    check_i1 = -1;
-    check_d0 = -1;
-    check_d1 = -1;
-    tinycli_putsn("i 0x13\r", 7);   /* write command string */
-    assert(check_i0 == 0x13); /* test check variables */
-    assert(check_i1 == -1);
-    assert(check_d0 == -1);
-    assert(check_d1 == -1);
-   
-
-    /* Test double function */
-    check_i0 = -1;            /* init check variables */
-    check_i1 = -1;
-    check_d0 = -1;
-    check_d1 = -1;
-    tinycli_putsn("d 1.3\r", 6);    /* write command string */
-    assert(check_i0 == -1);   /* test check variables */
-    assert(check_i1 == -1);
-    assert(check_d0 == 1.3);
-    assert(check_d1 == -1);
-   
-
-    /* Test int,int function */
-    check_i0 = -1;            /* init check variables */
-    check_i1 = -1;
-    check_d0 = -1;
-    check_d1 = -1;
-    tinycli_putsn("ii 12 -5\r", 9); /* write command string */
-    assert(check_i0 == 12);   /* test check variables */
-    assert(check_i1 == -5);
-    assert(check_d0 == -1);
-    assert(check_d1 == -1);
-
-    
-    /* Test int,double function */
-    check_i0 = -1;            /* init check variables */
-    check_i1 = -1;
-    check_d0 = -1;
-    check_d1 = -1;
-    tinycli_putsn("id 12 -0.123\r", 13);
-    assert(check_i0 == 12);   /* test check variables */
-    assert(check_i1 == -1);
-    assert(check_d0 == -1);
-    assert(check_d1 == -0.123);
-
-    /* Test double,int function */
-    check_i0 = -1;            /* init check variable */
-    check_i1 = -1;
-    check_d0 = -1;
-    check_d1 = -1;
-    tinycli_putsn("di 123.0 12\r", 12);
-    assert(check_i0 == -1);   /* test check variables */
-    assert(check_i1 == 12);
-    assert(check_d0 == 123.0);
-    assert(check_d1 == -1);
-
-    /* Test double,double function */
-    check_i0 = -1;            /* init check variable */
-    check_i1 = -1;
-    check_d0 = -1;
-    check_d1 = -1;
-    tinycli_putsn("dd 123.0 12\r", 12);
-    assert(check_i0 == -1);   /* test check variables */
-    assert(check_i1 == -1);
-    assert(check_d0 == 123.0);
-    assert(check_d1 == 12.0);
-    
-    /* Test string function */
-    check_i0 = -1;            /* init check variable */
-    check_i1 = -1;
-    check_d0 = -1;
-    check_d1 = -1;
-    check_s0 = -1;
-    check_s1 = -1;
-    tinycli_putsn("s helloWorld\r", 13);
-    assert(check_s0 == 0);   /* test check variables */
-    assert(check_s1 > 0);
-
-    /* Test no command */
-    check_i0 = -1;            /* init check variable */
-    check_i1 = -1;
-    check_d0 = -1;
-    check_d1 = -1;
-    check_s0 = -1;
-    check_s1 = -1;
-    tinycli_putsn("\r", 1);
-    assert(check_s0 == -1);   /* test check variables */
-    assert(check_s1 == -1);
-
+    for (int i = 0; i < sizeof(tests)/sizeof(testcase); i++) {
+        testcase t = tests[i];
+        printf("Test: %s ... ", t.testname);
+        runTest(t);
+        printf("PASS\n");
+    }
     return 0;
 }
---
+++ test/testfunctions.h
@@ -0,0 +1,78 @@
+
+int f_v(void) {
+    return 1;
+}
+
+int f_i(int arg0) {
+    argsInt[0] = arg0;
+    return 2;
+}
+
+int f_d(double arg0) {
+    argsDouble[0] = arg0;
+    return 3;
+}
+
+int f_ii(int arg0, int arg1) {
+    argsInt[0] = arg0;
+    argsInt[1] = arg1;
+    return 4;
+}
+
+int f_id(int arg0, double arg1) {
+    argsInt[0] = arg0;
+    argsDouble[1] = arg1;
+    return 5;
+}
+
+int f_di(double arg0, int arg1) {
+    argsDouble[0] = arg0;
+    argsInt[1] = arg1;
+    return 6;
+}
+
+int f_dd(double arg0, double arg1) {
+    argsDouble[0] = arg0;
+    argsDouble[1] = arg1;
+    return 7;
+}
+
+int f_s(char* arg0) {
+    strcpy(argsStr[0], arg0);
+    return 8;
+}
+
+int f_ll(long long arg0) {
+    return 0;
+}
+
+int f_iiiiiiiiii(int arg0, int arg1, int arg2, int arg3, int arg4,
+                 int arg5, int arg6, int arg7, int arg8, int arg9) {
+    argsInt[0] = arg0;
+    argsInt[1] = arg1;
+    argsInt[2] = arg2;
+    argsInt[3] = arg3;
+    argsInt[4] = arg4;
+    argsInt[5] = arg5;
+    argsInt[6] = arg6;
+    argsInt[7] = arg7;
+    argsInt[8] = arg8;
+    argsInt[9] = arg9;
+    return 0;
+}
+
+int f_iiiiiiiiiii(int arg0, int arg1, int arg2, int arg3, int arg4,
+                  int arg5, int arg6, int arg7, int arg8, int arg9, int arg10) {
+    argsInt[0] = arg0;
+    argsInt[1] = arg1;
+    argsInt[2] = arg2;
+    argsInt[3] = arg3;
+    argsInt[4] = arg4;
+    argsInt[5] = arg5;
+    argsInt[6] = arg6;
+    argsInt[7] = arg7;
+    argsInt[8] = arg8;
+    argsInt[9] = arg9;
+    return 0;
+}
+
